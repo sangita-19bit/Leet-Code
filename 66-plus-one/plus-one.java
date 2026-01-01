b594e690040bf381e70ddfb0b6e74f141c2f4536
sangita-19bit <sangitabeauty67@gmail.com>
2026-01-01 22:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (37.47%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int add = 1;
+        int sum = 0;
+        for(int i= n-1;i>=0 ;i--){
+            if(digits[i]<9){
+                digits[i] = digits[i]+1;
+                return digits;
+            }
+            else{
+                digits[i]=0;
+
+            } 
+        }
+        int arr[]=new int [n+1];
+        arr[0]=1;
+        
+       return arr;
+    }
+    
+}
